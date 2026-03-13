09a9ec1ef7836dd9c16da914d37ac65f3aecbd90
Youcef <youcef68001@gmail.com>
2026-03-13 18:43:50 -0400

    h2 console fix

---
+++ src/main/java/com/champsoft/vrms2330097/modules/agents/domain/exception/InvalidAgentNameException.java
@@ -0,0 +1,6 @@
+package com.champsoft.vrms2330097.modules.agents.domain.exception;
+
+public class InvalidAgentNameException extends RuntimeException {
+    public InvalidAgentNameException(String message) { super(message); }
+}
+
